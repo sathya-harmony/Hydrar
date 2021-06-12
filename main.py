e7fb1a8a5f52755084d2be2e80b84040f05eeee4
sathya-harmony <sathya.shrik@gmail.com>
2021-06-12 19:03:30 +0530

    f

--- main.py
+++ main.py
@@ -47,8 +47,8 @@ intents.members = True
 
 @client.event
 async def on_member_join(member):
-    guild = client.get_guild(846947170782281732)
-    channel = guild.get_channel(847508482454323270)
+    guild = client.get_guild(846947170782281729)
+    channel = guild.get_channel(846947170782281732)
     intro = guild.get_channel(847508482454323270)
     await channel.send(f"Welcome to {guild.name} {member.mention}! Please Introduce yourself in {intro}")
 
