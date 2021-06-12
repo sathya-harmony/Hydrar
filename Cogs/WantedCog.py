c702972f35d57a8501ba40ff9495bc545ed0ea71
sathya-harmony <sathya.shrik@gmail.com>
2021-06-12 10:15:03 +0530

    lol

--- Cogs/WantedCog.py
+++ Cogs/WantedCog.py
@@ -11,6 +11,7 @@ client = commands.Bot(command_prefix=prefix,
                       case_insensitive=True,
                       intents=discord.Intents.all())
 
+
 @commands.Cog.listener()
 async def on_command_error(ctx, error):
     if isinstance(error, commands.UserInputError):
@@ -23,49 +24,49 @@ async def on_command_error(ctx, error):
     elif isinstance(error, commands.CommandNotFound):
         await ctx.send("Invalid command.")
 
+
 class Wanted(commands.Cog):
     def __init__(self, client):
         self.client = client
-    
+
     @commands.command()
     async def wanted(self, ctx, user: discord.Member = None):
-      if user == None:
-        user = ctx.author
+        if user == None:
+            user = ctx.author
 
-      wanted = Image.open("Cogs/Pics/Wanted.jpg")
-      asset = user.avatar_url_as(size = 128)
-      data = BytesIO(await asset.read())
-      pfp = Image.open(data)  
-      pfp = pfp.resize((318,318))
-      wanted.paste(pfp, (125,256))
-      wanted.save("ReturnPICS/profile.jpg")
-      await ctx.send(file = discord.File("ReturnPICS/profile.jpg"))
+        wanted = Image.open("Cogs/Pics/Wanted.jpg")
+        asset = user.avatar_url_as(size=128)
+        data = BytesIO(await asset.read())
+        pfp = Image.open(data)
+        pfp = pfp.resize((318, 318))
+        wanted.paste(pfp, (125, 253))
+        wanted.save("ReturnPICS/profile.jpg")
+        await ctx.send(file=discord.File("ReturnPICS/profile.jpg"))
 
     @commands.command()
     async def rip(self, ctx, user: discord.Member = None):
-      if user == None:
-        user = ctx.author
+        if user == None:
+            user = ctx.author
 
-      rip = Image.open("Cogs/Pics/RIP.jpg")      
-      asset = user.avatar_url_as(size = 128)
-      data = BytesIO(await asset.read())
-      pfp = Image.open(data)    
-      pfp = pfp.resize((142, 125))      
-      rip.paste(pfp, (81, 145))
-      rip.save("ReturnPICS/rip.jpg")
-      await ctx.send(file = discord.File("ReturnPICS/rip.jpg"))
-
+        rip = Image.open("Cogs/Pics/RIP.jpg")
+        asset = user.avatar_url_as(size=128)
+        data = BytesIO(await asset.read())
+        pfp = Image.open(data)
+        pfp = pfp.resize((142, 125))
+        rip.paste(pfp, (81, 145))
+        rip.save("ReturnPICS/rip.jpg")
+        await ctx.send(file=discord.File("ReturnPICS/rip.jpg"))
 
     @commands.command()
     async def avatar(self, ctx, user: discord.Member = None):
-      if user == None:
-        await ctx.send("Mention somebody to get their Avatar.")
+        if user == None:
+            await ctx.send("Mention somebody to get their Avatar.")
 
-      else:
-        embed = discord.Embed(title=f"{user}'s Avatar!", color = ctx.author.color, timestamp=ctx.message.created_at)   
-        embed.set_image(url = f"{user.avatar_url}")
-        await ctx.send(embed = embed)
-      
+        else:
+            embed = discord.Embed(
+                title=f"{user}'s Avatar!", color=ctx.author.color, timestamp=ctx.message.created_at)
+            embed.set_image(url=f"{user.avatar_url}")
+            await ctx.send(embed=embed)
 
 
 def setup(client):
