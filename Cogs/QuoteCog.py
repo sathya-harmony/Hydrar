731b44692a7655c55bf449793d6fddf6da6cf9f3
sathya-harmony <sathya.shrik@gmail.com>
2021-06-12 12:52:31 +0530

    lol

--- Cogs/QuoteCog.py
+++ Cogs/QuoteCog.py
@@ -5,10 +5,12 @@ import json
 import ast
 import asyncio
 from discord.ext import commands
+import pyjokes
 prefix = '-'
 client = commands.Bot(command_prefix=prefix,
                       case_insensitive=True,
                       intents=discord.Intents.all())
+
 
 @commands.Cog.listener()
 async def on_command_error(ctx, error):
@@ -20,19 +22,17 @@ async def on_command_error(ctx, error):
     elif isinstance(error, commands.MissingRequiredArgument):
         await ctx.send('Please give proper input.')
     elif isinstance(error, commands.CommandNotFound):
-        await ctx.send("Invalid command.")                      
+        await ctx.send("Invalid command.")
 
 
 class Quote(commands.Cog):
     def __init__(self, client):
         self.client = client
 
-    
     @commands.command()
-    
-    async def quote(self, ctx):  
+    async def quote(self, ctx):
         async with ctx.channel.typing():
-            await asyncio.sleep(0.5)    
+            await asyncio.sleep(0.5)
         response = requests.get("https://py-quoters.herokuapp.com/")
         wl = response.text
         quod = ast.literal_eval(wl)
@@ -40,6 +40,12 @@ class Quote(commands.Cog):
 
         await ctx.send(f"**Here's a Quote to Inspire you:**\n{quote}")
 
-      
+    @commands.command()
+    async def joke(self, ctx):
+        async with ctx.channel.typing():
+            await asyncio.sleep(0.5)
+        await ctx.send((pyjokes.get_joke()))
+
+
 def setup(client):
     client.add_cog(Quote(client))
