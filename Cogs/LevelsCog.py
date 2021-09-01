6359826ec66e4136c4909f6bff7879caa5d0dd79
sathya-harmony <sathya.shrik@gmail.com>
2021-09-01 09:37:26 +0530

    f

--- Cogs/LevelsCog.py
+++ Cogs/LevelsCog.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 from pymongo import MongoClient
 
 
-bot_channel = [870541730410270814]
+bot_channel = 870541730410270814
 talk_channel = [870541730410270814]
 level = ["Level 1"]
 levelnum = [2]
