55c0b235055888f48b29b4f9df061c35d58a6799
sathya-harmony <sathya.shrik@gmail.com>
2021-10-01 20:22:04 +0530

    f

--- Cogs/EconomyCog.py
+++ Cogs/EconomyCog.py
@@ -203,6 +203,14 @@ class Economy(commands.Cog):
                 salary = int(self.job_list[job_name]['salary'])
                 cut_off = random.choice([1.5, 1.75, 1.96, 1.99, 2.12, 2.25])
                 amount = int(salary / cut_off)
+            elif job_name in self.job_list_2:    
+                salary = int(self.job_list_2[job_name]['salary'])
+                cut_off = random.choice([1.5, 1.75, 1.96, 1.99, 2.12, 2.25])
+                amount = int(salary / cut_off)
+            elif job_name in self.job_list_3:
+                salary = int(self.job_list_3[job_name]['salary'])
+                cut_off = random.choice([1.5, 1.75, 1.96, 1.99, 2.12, 2.25])
+                amount = int(salary / cut_off)    
             embed = discord.Embed(title=f"Terrible Effort, {ctx.author.mention}! I expected better work from you :angry:",
                                   description=f'You lost the mini-game because you ran out of time.\nYou were given {amount} for a sub-par hour of work.')
             
