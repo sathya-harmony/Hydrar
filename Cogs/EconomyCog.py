6f5691f8496d43f5389e97e748b0f172071edb72
sathya-harmony <sathya.shrik@gmail.com>
2021-10-02 14:02:28 +0530

    f

--- Cogs/EconomyCog.py
+++ Cogs/EconomyCog.py
@@ -349,10 +349,12 @@ class Economy(commands.Cog):
         interaction = await self.client.wait_for("button_click", timeout=15.0)
         i = 0
         for items in emoji_waste_list:
-            i+=1            
+                        
             if interaction.component.emoji == str(items[i]):
                 await ctx.send("gg")
                 break
+            else:
+                i+=1
                
             
 
