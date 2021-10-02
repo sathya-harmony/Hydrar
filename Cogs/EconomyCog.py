fae562ab594e7c3ff27ddb69ad7abbaee2bcd6b7
sathya-harmony <sathya.shrik@gmail.com>
2021-10-02 20:07:20 +0530

    f

--- Cogs/EconomyCog.py
+++ Cogs/EconomyCog.py
@@ -360,8 +360,8 @@ class Economy(commands.Cog):
                     row2.append(Button(label = emoji_waste_list[x], style = ButtonStyle.green, disabled = True))
                   else: 
                       row2.append(Button(label = emoji_waste_list[x], disabled = True))
-            await interaction.message.edit(components = [row1, row2])
-            await interaction.respond(type=6)           
+            await interaction.edit_origin(components = [row1, row2])
+            #await interaction.respond(type=6)           
 
             
         '''buttons1 = []        
