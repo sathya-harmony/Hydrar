1c4166a6d4ba9d20fad68021fd2320652e5beddc
sathya-harmony <sathya.shrik@gmail.com>
2021-10-08 17:05:59 +0530

    f

--- main.py
+++ main.py
@@ -403,7 +403,7 @@ async def _do(ctx, *, code):
     try:
         with contextlib.redirect_stdout(stdout):
             exec(
-                f"async def func():\n{textwrap.indent(code, '    ')}", local_variables,
+                f"async def func():\n {textwrap.indent(code, '    ')}", local_variables,
             )
 
             obj = await local_variables["func"]()
