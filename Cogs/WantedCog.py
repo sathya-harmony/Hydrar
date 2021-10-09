bf6dde0c505e6c6220c64703d18fcebf193a2ace
sathya-harmony <sathya.shrik@gmail.com>
2021-10-09 14:46:23 +0530

    d

--- Cogs/WantedCog.py
+++ Cogs/WantedCog.py
@@ -206,7 +206,7 @@ class Wanted(commands.Cog):
         embed = discord.Embed(
             title=":zzz: Member AFK", description=f"{member.mention} has gone **AFK**", color=member.color)
         embed.set_thumbnail(url=member.avatar_url)
-        embed.set_footer(name=self.client.user.name,
+        embed.set_footer(text = self.client.user.name,
                          icon_url=self.client.user.avatar_url)
         embed.add_field(name='AFK Note:', value=reason)
         await ctx.message.reply(embed=embed)
