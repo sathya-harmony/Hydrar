3f70a82b1af70ac5fc28579c6e34caac6c4e6894
sathya-harmony <sathya.shrik@gmail.com>
2021-10-10 22:56:04 +0530

    d

--- Cogs/HelpCog_test.py
+++ Cogs/HelpCog_test.py
@@ -13,7 +13,7 @@ class _help_(commands.Cog):
 
     @commands.command(name='help',
                       aliases=["helpme", "command", "commands", "cmd", "cmds"])
-    async def help(self, ctx, arg):
+    async def help(self, ctx, arg=''):
 
         embed1 = discord.Embed(
             title="Hydrargyrum - Command Categories",
