b0d4109f9f55c6e1f536d4b287cc68e6446cb52e
sathya-harmony <sathya.shrik@gmail.com>
2021-10-14 12:34:12 +0530

    d

--- main.py
+++ main.py
@@ -131,7 +131,7 @@ async def on_message_delete(message):
 @client.event
 async def on_message(msg):
     try:
-        if client.user.id in (member.id for member in msg.mentions):
+        if client.user.id == (member.id for member in msg.mentions):
             Prefixes = Prefixes_MongoDB.find_one(
                 {"guild_id": str(msg.guild.id)})
             if Prefixes is None:
