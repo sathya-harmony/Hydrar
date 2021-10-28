2a5444ad251c8af16b4359bffdc7dd65cace6af0
sathya-harmony <sathya.shrik@gmail.com>
2021-10-28 21:18:00 +0530

    d

--- Cogs/WantedCog.py
+++ Cogs/WantedCog.py
@@ -59,7 +59,7 @@ class Wanted(commands.Cog):
                 avatar_bytes = io.BytesIO(await resp.read())
         ava = Image.open(avatar_bytes)
         ava = ava.resize((142, 125))
-        rip.paste(ava, (122, 251))
+        rip.paste(ava, (120, 150))
         rip.save("image.jpg")
         await ctx.message.reply(file=discord.File("image.jpg"))
 
