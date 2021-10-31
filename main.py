7c7241db9bb3d7ac0d4b6a7ed26d775e0d3e4cd5
sathya-harmony <sathya.shrik@gmail.com>
2021-10-31 23:02:39 +0530

    d

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ from discord.ext.buttons import Paginator
 from modules.common import *
 import threading
 #from discord.ext import ipc
-from discord_slash_components_bridge import SlashCommand
+#from discord_slash_components_bridge import SlashCommand
 
 
 print('Hydrargyrum is loading...')
@@ -78,7 +78,7 @@ client = commands.Bot(command_prefix=get_prefix,
                       strip_after_prefix=True,
                       case_insensitive=True,
                       intents=discord.Intents.all())
-slash = SlashCommand(client, sync_commands=True)
+#slash = SlashCommand(client, sync_commands=True)
 
 client.remove_command('help')
 
@@ -170,7 +170,7 @@ async def snipe(ctx):
     await ctx.message.reply(embed=embed)
 
 
-@slash.slash(name='Snipe', description="Retreieve the most recently deleted message!")
+'''@slash.slash(name='Snipe', description="Retreieve the most recently deleted message!")
 async def snipe(ctx):
     try:
         contents, author, channel_name, time = client.sniped_messages[ctx.guild.id]
@@ -185,8 +185,7 @@ async def snipe(ctx):
         name=f"{author.name}#{author.discriminator}", icon_url=author.avatar_url)
     embed.set_footer(text=f"Deleted in : #{channel_name}")
 
-    await ctx.send(embed=embed)
-
+    await ctx.send(embed=embed)'''
 
 # Log Channel.
 log_channel = None
