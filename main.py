c58ab9b5c7feb6f6ce7c434e3b2b243b361398d4
sathya-harmony <sathya.shrik@gmail.com>
2021-11-01 09:40:45 +0530

    f

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ import textwrap
 import contextlib
 from traceback import format_exception
 from discord.ext.buttons import Paginator
-#from discord_slash.client import SlashCommand
+import discord_slash
 from modules.common import *
 import threading
 #from discord.ext import ipc
@@ -79,6 +79,8 @@ client = commands.Bot(command_prefix=get_prefix,
                       case_insensitive=True,
                       intents=discord.Intents.all())
 #slash = SlashCommand(client, sync_commands=True)
+DiscordComponents(client)
+slash = discord_slash.SlashCommand(client, sync_commands=True)
 
 client.remove_command('help')
 
@@ -170,7 +172,7 @@ async def snipe(ctx):
     await ctx.message.reply(embed=embed)
 
 
-'''@slash.slash(name='Snipe', description="Retreieve the most recently deleted message!")
+@slash.slash(name='Snipe', description="Retreieve the most recently deleted message!")
 async def snipe(ctx):
     try:
         contents, author, channel_name, time = client.sniped_messages[ctx.guild.id]
@@ -185,7 +187,7 @@ async def snipe(ctx):
         name=f"{author.name}#{author.discriminator}", icon_url=author.avatar_url)
     embed.set_footer(text=f"Deleted in : #{channel_name}")
 
-    await ctx.send(embed=embed)'''
+    await ctx.send(embed=embed)
 
 # Log Channel.
 log_channel = None
