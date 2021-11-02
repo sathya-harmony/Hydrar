9a6beeb42b0ce2b494f6d86936de3026688fcd5f
sathya-harmony <sathya.shrik@gmail.com>
2021-11-02 21:03:56 +0530

    f

--- Cogs/WantedCog.py
+++ Cogs/WantedCog.py
@@ -61,7 +61,7 @@ class Wanted(commands.Cog):
 
         ava = Image.open(avatar_bytes)
         ava = ava.resize((200, 195))
-        delete.paste(ava, (120, 150))
+        delete.paste(ava, (120, 140))
         delete.save("delete.png")
         await ctx.message.reply(file=discord.File("delete.png"))
 
