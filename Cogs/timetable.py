3f5ede66eabadb252de122d2b4746d55b4cb2d5f
sathya-harmony <sathya.shrik@gmail.com>
2021-12-13 16:12:39 +0530

    f

--- Cogs/timetable.py
+++ Cogs/timetable.py
@@ -82,7 +82,8 @@ class timetable(commands.Cog):
 
     checktimetable.start()
 
-    @commands.Cog.listener()
+    @ commands.Cog.listener()
+    # async def on_message(self, message):
     async def check_subject(self):
         channel = await self.client.get_channel(self.channel_id)
         await channel.send(f"It's **{self.subject}** period! Please join right now! (P.S it might be substitution so don't blast me.)")
