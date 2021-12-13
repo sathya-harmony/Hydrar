74eb6b7601a218a62132bfd44cf109e7201fa217
sathya-harmony <sathya.shrik@gmail.com>
2021-12-13 16:45:54 +0530

    f

--- Cogs/timetable.py
+++ Cogs/timetable.py
@@ -84,7 +84,8 @@ class Timetable(commands.Cog):
             pass
         if self.subject is not None:
             channel = self.client.get_channel(self.channel_id)
-            await channel.send(f"It's **{self.subject}** period! Please join right now! (P.S it might be substitution so don't blast me.)")
+            channel.send(
+                f"It's **{self.subject}** period! Please join right now! (P.S it might be substitution so don't blast me.)")
             self.subject = None
 
 
